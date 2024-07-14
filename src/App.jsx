9a4ac271707a8dc3c28835f9017154d25a6ac5f2
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-14 15:53:09 -0300

    Add about component content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Info from "./components/Info"
+import About from "./components/About"
 
 
 function App() {
   return (
     <div className="container">
       <Info />
+      <About />
     </div>
   )
 }
