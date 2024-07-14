24e9afb9879674922c5c51a55fba0766db2a4a78
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-14 17:23:23 -0300

    Style

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,3 @@ function App() {
 }
 
 export default App
-
-
-/*
-Info (photo, name, buttons)
-About
-Interests
-footer (social icons)
-
-*/
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,24 +1,22 @@
-import profilePicture from '../assets/profile-picture.jpeg'
+import profilePicture from '../assets/profile-picture-2.jpeg'
 import emailIcon from '../assets/email-icon.png'
 import linkedinIcon from '../assets/linkedin-icon.png'
 
 function Info() {
     return (
         <div className="info">
-            <div className="info--profile-picture">
-                <img src={profilePicture} alt="Snoopy" />
-            </div>
+            <img className="info--profile-picture" src={profilePicture} alt="Snoopy" />
             <div className="info--basic-info">
-                <h1 className="name">Paulo M Borges</h1>
+                <h1 className="name">Snoopy Smith</h1>
                 <h2 className="title">Frontend Developer</h2>
-                <p className="website">paulomborges.website</p>
+                <p className="website">snoopysmith.website</p>
             </div>
             <div className="info--social">
-                <button>
+                <button className='email-btn'>
                     <img src={emailIcon} alt="email icon" />
                     Email
                 </button>
-                <button>
+                <button className='linkedin-btn'>
                     <img src={linkedinIcon} alt="linkedin icon" />
                     Linkedin
                 </button>
